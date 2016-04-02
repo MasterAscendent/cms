4c78ebe52039f0b5ed62aee202681bdc92ac8b9f
Steven Hensley II <stevehensley2@gmail.com>
2016-04-01 19:59:40 -0700

    #106. Creating an HTML table in admin to display a list of posts.

---
+++ admin/posts.php
@@ -0,0 +1,63 @@
+<?php include "includes/admin_header.php"; ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include "includes/admin_navigation.php"; ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+
+                      <h1 class="page-header">
+                          WELCOME TO ADMIN
+                          <small>Author</small>
+                      </h1>
+
+                      <table class="table table-border table-hover">
+
+                        <thead>
+                          <th>Id</th>
+                          <th>Author</th>
+                          <th>Title</th>
+                          <th>Category</th>
+                          <th>Status</th>
+                          <th>Image</th>
+                          <th>Tags</th>
+                          <th>Comments</th>
+                          <th>Date</th>
+                        </thead>
+
+                        <tbody>
+                          <tr>
+                            <td>10</td>
+                            <td>Steven Hensley II</td>
+                            <td>Bootstrap framework</td>
+                            <td>Bootstrap</td>
+                            <td>Status</td>
+                            <td>Image</td>
+                            <td>Tages</td>
+                            <td>Comments</td>
+                            <td>Date</td>
+                          </tr>
+                        </tbody>
+
+                      </table>
+
+
+
+                    </div>
+                <!-- /.row -->
+
+                </div>
+            <!-- /.container-fluid -->
+
+            </div>
+        <!-- /#page-wrapper -->
+        </div>
+      </div>
+<?php include "includes/admin_footer.php" ?>
