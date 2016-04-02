00a4a098c6f02b535d5dea002b4734211f7a0367
Steven Hensley II <stevehensley2@gmail.com>
2016-04-01 20:33:58 -0700

    #108. Including Pages based on condition technique.

--- admin/posts.php
+++ admin/posts.php
@@ -17,76 +17,42 @@
                           WELCOME TO ADMIN
                           <small>Author</small>
                       </h1>
+                      <?php
 
-                      <table class="table table-border table-hover">
+                        if(isset($_GET['source'])) {
 
-                        <thead>
-                          <th>Id</th>
-                          <th>Author</th>
-                          <th>Title</th>
-                          <th>Category</th>
-                          <th>Status</th>
-                          <th>Image</th>
-                          <th>Tags</th>
-                          <th>Comments</th>
-                          <th>Date</th>
-                        </thead>
+                          $source = $_GET['source'];
 
-                        <tbody>
+                        } else {
 
+                          $source = '';
 
-                          <?php
+                        }
+                        switch ($source ) {
+                          case '34';
+                            echo "NICE 34";
+                            break;
 
-                          $query = "SELECT * FROM posts";
-                          $select_posts = mysqli_query($connection, $query);
+                          case '100';
+                            echo "NICE 100";
+                            break;
 
-                          while($row = mysqli_fetch_assoc($select_posts)) {
-                            $post_id = $row['post_id'];
-                            $post_author = $row['post_author'];
-                            $post_title = $row['post_title'];
-                            $post_category_id = $row['post_category_id'];
-                            $post_status = $row['post_status'];
-                            $post_image = $row['post_image'];
-                            $post_tags = $row['post_tags'];
-                            $post_comment_count = $row['post_comment_count'];
-                            $post_date = $row['post_date'];
+                          case '200';
+                            echo "NICE 200";
+                            break;
 
-                            echo "<tr>";
-                            echo "<td>$post_id</td>";
-                            echo "<td>$post_author</td>";
-                            echo "<td>$post_title</td>";
-                            echo "<td>$post_category_id</td>";
-                            echo "<td>$post_status</td>";
-                            echo "<td><img width='100px' src='../images/$post_image' alt='image'/td>";
-                            echo "<td>$post_tags</td>";
-                            echo "<td>$post_comment_count</td>";
-                            echo "<td>$post_date</td>";
-                            echo "</tr>";
+                          default:
+                            include "includes/view_all_posts.php";
+                            // CODE HERE
+                            break;
+                        }
 
 
 
-                          }
-
-                           ?>
 
 
 
-                            <td>10</td>
-                            <td>Steven Hensley II</td>
-                            <td>Bootstrap framework</td>
-                            <td>Bootstrap</td>
-                            <td>Status</td>
-                            <td>Image</td>
-                            <td>Tages</td>
-                            <td>Comments</td>
-                            <td>Date</td>
-                          </tr>
-                        </tbody>
-
-                      </table>
-
-
-
+                       ?>
                     </div>
                 <!-- /.row -->
 
