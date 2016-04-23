cf4524acf9c82eb01a11885d01ec58c447f2acad
Steven Hensley II <stevehensley2@gmail.com>
2016-04-23 09:18:25 -0700

    #156. Logout page improved validation.

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -4,12 +4,9 @@
 <?php include "functions.php"; ?>
 
 <?php
-  if(isset($_SESSION['user_role'])) {
-    if($_SESSION['user_role'] !== 'admin') {
+  if(!isset($_SESSION['user_role'])) {
 
       header("Location: ../index.php");
-
-    }
 
   }
 
