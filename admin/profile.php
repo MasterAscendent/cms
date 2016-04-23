2e4bd17d5ce1a2ee3c439f47622d6939091bd0ad
Steven Hensley II <stevehensley2@gmail.com>
2016-04-23 09:37:15 -0700

    #158. Creating the profile page.

--- admin/profile.php
+++ admin/profile.php
@@ -1,6 +1,13 @@
 <?php include "includes/admin_header.php"; ?>
 
+<?php
+if(isset($_SESSION['username'])) {
 
+
+
+}
+
+ ?>
 
 
     <div id="wrapper">
@@ -20,14 +27,70 @@
                           WELCOME TO ADMIN
                           <small>Author</small>
                       </h1>
-                      <?php
-                      if(isset($_SESSION['username'])) {
 
-                        echo $_SESSION['username'];
+                      <form action="" method="post" enctype="multipart/form-data">
 
-                      }
+                        <div class="form-group">
+                          <label for="title">Firstname</label>
+                          <input type="text" value="<?php echo $user_firstname; ?>" class="form-control" name="user_firstname">
+                        </div>
 
-                       ?>
+                        <div class="form-group">
+                          <label for="post_status">Lastname</label>
+                          <input type="text" value="<?php echo $user_lastname; ?>" class="form-control" name="user_lastname">
+                        </div>
+                      
+                        <div class="form-group">
+                          <select name="user_role" id="">
+
+                            <option value="subscriber"><?php echo $user_role; ?></option>
+
+                            <?php
+
+                              if($user_role == 'admin') {
+
+                                echo "<option value='subscriber'>subscriber</option>";
+
+                              } else {
+
+                                echo "<option value='admin'>admin</option>";
+
+                              }
+
+                             ?>
+
+                          </select>
+                        </div>
+
+
+                        <!-- <div class="form-group">
+                          <label for="post_image">zxzxczxczxczc</label>
+                          <input type="file" name="image">
+                        </div> -->
+
+                        <div class="form-group">
+                          <label for="post_tags">Username</label>
+                          <input type="text" value="<?php echo $username; ?>" class="form-control" name="username">
+                        </div>
+
+
+                        <div class="form-group">
+                          <label for="post_tags">Email</label>
+                          <input type="email" value="<?php echo $user_email; ?>" class="form-control" name="user_email">
+                        </div>
+
+                        <div class="form-group">
+                          <label for="post_tags">Password</label>
+                          <input type="password" value="<?php echo $user_password; ?>" class="form-control" name="user_password">
+                        </div>
+
+                        <div class="form-group">
+                          <input class="btn btn-primary" type="submit" name="edit_user" value="Edit User">
+                        </div>
+
+                      </form>
+
+
                     </div>
                 <!-- /.row -->
 
