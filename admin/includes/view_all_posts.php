7952c59c47531224461f792c0a0bef3d5e3daf43
Steven Hensley II <stevehensley2@gmail.com>
2016-04-26 08:29:59 -0700

    #182. Adding bulk options post, part 5.

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -14,7 +14,27 @@
 
           confirmQuery($update_to_published_status);
 
-          break;
+        break;
+
+        case 'draft':
+
+          $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id= {$postValueId} ";
+
+          $update_to_draft_status = mysqli_query($connection, $query);
+
+          confirmQuery($update_to_draft_status);
+
+        break;
+
+        case 'delete':
+
+          $query = "DELETE FROM posts WHERE post_id = {$postValueId} ";
+
+          $update_to_delete_status = mysqli_query($connection, $query);
+
+          confirmQuery($update_to_delete_status);
+
+        break;
       }
 
     }
