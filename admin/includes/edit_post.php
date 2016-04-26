3f9c683cf6f8cea71646b7926a29a979efd2ed93
Steven Hensley II <stevehensley2@gmail.com>
2016-04-25 17:19:09 -0700

    #176. Adding post update notification.

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -65,6 +65,8 @@ while($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
     confirmQuery($update_post);
 
+    echo "<p class='bg-success'>Post Updated. <a href='../post.php?p_id={$the_post_id}'> View Post</a> or <a href='posts.php'> Edit More Posts</a></p>";
+
   }
 
  ?>
