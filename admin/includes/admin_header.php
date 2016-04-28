93108fb52b67d98c3e6483b31681c140953b06b5
Steven Hensley II <stevehensley2@gmail.com>
2016-04-28 13:14:54 -0700

    #207. Adding a loader to the CMS admin.

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -42,6 +42,10 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <!-- Custom CSS -->
+    <link href="css/styles.css" rel="stylesheet">
+
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script src="http://cdn.tinymce.com/4/tinymce.min.js"></script>
 <script src="js/scripts.js"></script>
--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -43,4 +43,21 @@ $(document).ready(function() {
 
   });
 
+  var div_box = "<div id='load-screeen'><div id='loading'></div></div>";
+
+  $("body").prepend(div_box);
+
+  $('#load-screen').delay(700).fadeOut(600, function(){
+    $(this).remove();
+  });
+
+
+
+
+
+
+
+
+
+
 });
