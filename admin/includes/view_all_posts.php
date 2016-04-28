39b842e04da06a9e9a8eaea7d0706c1cc5ec6dd6
Steven Hensley II <stevehensley2@gmail.com>
2016-04-28 13:38:40 -0700

    #209. Resetting views feature.

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -168,7 +168,7 @@
       echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
       echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
       echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to Delete?');\" href='posts.php?delete={$post_id}'>Delete</a></td>";
-      echo "<td>{$post_view_count}</td>";
+      echo "<td><a href='posts.php?reset={$post_id}'>{$post_view_count}</a></td>";
       echo "</tr>";
 
 
@@ -193,5 +193,16 @@ if(isset($_GET['delete'])) {
   header("Location: posts.php");
 
 }
+
+if(isset($_GET['reset'])) {
+
+  $the_post_id = $_GET['reset'];
+
+  $query = "UPDATE posts SET post_view_count = 0 WHERE post_id =" . mysqli_real_escape_string($connection, $_GET['reset']) . " ";
+  $reset_query = mysqli_query($connection, $query);
+  header("Location: posts.php");
+
+}
+
 
  ?>
